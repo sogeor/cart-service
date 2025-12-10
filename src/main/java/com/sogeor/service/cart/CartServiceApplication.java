5c4544bfb1728357494f35a079621877550d0fd9
George <bloogefest@gmail.com>
2025-12-10 21:48:55 +0300

    Инициализация репозитория

---
+++ src/main/java/com/sogeor/service/cart/CartServiceApplication.java
@@ -0,0 +1,20 @@
+package com.sogeor.service.cart;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @since 1.0.0-RC1
+ */
+@SpringBootApplication
+public class CartServiceApplication {
+
+    /**
+     * @since 1.0.0-RC1
+     */
+    static void main(final @NotNull String... arguments) {
+        SpringApplication.run(CartServiceApplication.class, arguments);
+    }
+
+}
